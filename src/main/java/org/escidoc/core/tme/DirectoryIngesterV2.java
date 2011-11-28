e92f9cb38e39256f45c3eb5cd82fe8e5c2a53699
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2011-11-28 10:38:10 +0100

    update license header

--- src/main/java/org/escidoc/core/tme/DirectoryIngesterV2.java
+++ src/main/java/org/escidoc/core/tme/DirectoryIngesterV2.java
@@ -1,35 +1,32 @@
 /**
  * CDDL HEADER START
- * 
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License, Version 1.0 only (the "License"). You may not use
- * this file except in compliance with the License.
- * 
- * You can obtain a copy of the license at license/ESCIDOC.LICENSE or
- * https://www.escidoc.org/license/ESCIDOC.LICENSE . See the License for the
- * specific language governing permissions and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL HEADER in each file and
- * include the License file at license/ESCIDOC.LICENSE. If applicable, add the
- * following below this CDDL HEADER, with the fields enclosed by brackets "[]"
- * replaced with your own identifying information: Portions Copyright [yyyy]
- * [name of copyright owner]
- * 
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or https://www.escidoc.org/license/ESCIDOC.LICENSE .
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
  * CDDL HEADER END
- * 
- * 
- * 
- * Copyright 2011 Fachinformationszentrum Karlsruhe Gesellschaft fuer
- * wissenschaftlich-technische Information mbH and Max-Planck- Gesellschaft zur
- * Foerderung der Wissenschaft e.V. All rights reserved. Use is subject to
- * license terms.
+ *
+ *
+ *
+ * Copyright 2011 Fachinformationszentrum Karlsruhe Gesellschaft
+ * fuer wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Foerderung der Wissenschaft e.V.
+ * All rights reserved.  Use is subject to license terms.
  */
 package org.escidoc.core.tme;
-
-import com.google.common.base.Preconditions;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -41,6 +38,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
 
 import de.escidoc.core.client.exceptions.EscidocException;
 import de.escidoc.core.client.exceptions.InternalClientException;
--- src/test/java/org/escidoc/core/client/ingest/FileIngestV2Spec.java
+++ src/test/java/org/escidoc/core/client/ingest/FileIngestV2Spec.java
@@ -1,6 +1,39 @@
+/**
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or https://www.escidoc.org/license/ESCIDOC.LICENSE .
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *
+ * Copyright 2011 Fachinformationszentrum Karlsruhe Gesellschaft
+ * fuer wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Foerderung der Wissenschaft e.V.
+ * All rights reserved.  Use is subject to license terms.
+ */
 package org.escidoc.core.client.ingest;
 
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.util.Date;
 
 import org.escidoc.core.tme.FileIngesterV2;
 import org.escidoc.core.tme.SucessfulIngestResult;
@@ -9,11 +42,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.net.URI;
-import java.net.URL;
-import java.util.Date;
 
 import de.escidoc.core.client.Authentication;
 import de.escidoc.core.resources.common.reference.ContentModelRef;
